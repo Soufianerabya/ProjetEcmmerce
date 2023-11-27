eee69e7280eaa9518b6ab08203a7f3d07f54e72b
dounia <douniajammali@gmail.com>
2023-11-27 16:39:45 +0100

    first commit

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,24 @@
+import { createContext } from 'react';
+import all_product from'../Components/Assets/all_product';
+
+export const ShopContext = createContext(null);
+
+
+const ShopContextProvider = (props) => {
+  const allProducts =all_product;
+  allProducts.map((item)=>console.log(item.name)
+  )
+  
+
+  
+
+  
+
+  return (
+    <ShopContext.Provider value={allProducts}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
